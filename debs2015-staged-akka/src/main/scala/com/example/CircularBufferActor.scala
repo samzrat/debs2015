f3d3f46eb2b178a9a0c123141a0db742fb1b9207
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2015-02-22 23:18:51 -0700

    added time to circular buffer

--- debs2015-staged-akka/src/main/scala/com/example/CircularBufferActor.scala
+++ debs2015-staged-akka/src/main/scala/com/example/CircularBufferActor.scala
@@ -13,7 +13,7 @@ import scala.collection.mutable.Stack
 case class Cell (xCell: Int, yCell: Int)
 case class TripCells(startCell: Cell, endCell: Cell)
 case class TripEvent (startTime: Date, endTime: Date, grid500Cells: TripCells, grid250Cells: TripCells)
-case class BufferEntry(time: Option[Date], tripEventStack: Stack[TripEvent])
+class BufferEntry(var time: Option[Date], var tripEventStack: Stack[TripEvent]) {}
 
 class CircularBufferActor extends Actor with ActorLogging {
   import CircularBufferActor._
@@ -23,10 +23,10 @@ class CircularBufferActor extends Actor with ActorLogging {
   
   val circularBuffer: Array[BufferEntry] = new Array[BufferEntry](circularBufferSize)
   var head_30MinWindow: Int = -1
-  val tail_30MinWindow: Int = -1
+  var tail_30MinWindow: Int = -1
   
   var head_15MinWindow: Int = -1
-  val tail_15MinWindow: Int = -1
+  var tail_15MinWindow: Int = -1
   
   init()
 
@@ -45,94 +45,76 @@ class CircularBufferActor extends Actor with ActorLogging {
     val fileSource = Source.fromFile("sorted_data.csv")
     var newlineCount = 0L
     for (line <- fileSource.getLines) {
-/*      DataExtractor.extractTripEventData(line) match {
+      DataExtractor.extractTripEventData(line) match {
         case None =>
-        case Some(tripEvent) => {
+        case Some(tripEvent) => 
           routeCountActor ! RouteCountActor.IncrementRouteCountMsg(tripEvent)
-        }  
-      }
-  */    
- /*     
-
-        routeCountActor ! RouteCountActor.IncrementRouteCountMsg(tripEvent.get)
-        head.position match {
-          case -1 => 
-            head.position = 0
-            head.time = Some(tripEvent.get.endTime)
-            tail.position = 0
-            tail.time = Some(tripEvent.get.endTime)
-            println("Head= " + head.position + "      Tail= " + tail.position)
-          case  _ => 
-            ((tripEvent.get.endTime.getTime() - head.time.get.getTime())/1000) match {
-              case 0   => 
-                //println("Same data => head_time: " + head.time + ", incoming_trip_time: " + tripEvent.endTime)
-                circularBuffer(head.position).push(tripEvent.get)
-                head.time = Some(tripEvent.get.endTime)
-              case x if x > 0 => 
-                //println("X=" + x + " incoming_trip_time GREATER => head_time: " + head.time + ", incoming_trip_time: " + tripEvent.endTime)
-                
-                head.time = Some(tripEvent.get.endTime)
-                head.position = (head.position+x.toInt) % circularBufferSize
-                circularBuffer(head.position).push(tripEvent.get)
-                val oldTailPosition = tail.position 
-                //println("Date comparision = " + (head.time.get.getTime() - tail.time.get.getTime())/1000 + "            New Event date = " + tripEvent.endTime)
-                if((head.time.get.getTime() - tail.time.get.getTime())/1000 > circularBufferSize )
-                {
-                   val tailJump = ((head.time.get.getTime() - tail.time.get.getTime())/1000) - circularBufferSize
-                   tail.time.get.setTime(tail.time.get.getTime + tailJump.toInt*1000) 
-                   tail.position = (tail.position+tailJump.toInt) % circularBufferSize
-                }
-              if(oldTailPosition < tail.position-1) {
-                for(y <- oldTailPosition until tail.position) {
-                  val stackSize = circularBuffer(y).size
-                  for(z <- 0 to stackSize-1) {
-                    val event = circularBuffer(y).pop()
-                    //println("pop")
-                    routeCountActor ! RouteCountActor.DecrementRouteCountMsg(event)
+          head_30MinWindow match {
+            case -1 => 
+              assert (tail_30MinWindow== -1 && head_15MinWindow== -1 && tail_15MinWindow== -1, "not -1")
+              
+              head_30MinWindow = 0
+              tail_30MinWindow = 0
+              head_15MinWindow = 0
+              tail_15MinWindow = 0
+              
+              circularBuffer(0).time =  Some(tripEvent.endTime)
+              
+            case _ => 
+              ((tripEvent.endTime.getTime() - circularBuffer(head_30MinWindow).time.get.getTime())/1000) match {
+                case 0 =>
+                  circularBuffer(head_30MinWindow).tripEventStack.push(tripEvent)
+                  circularBuffer(head_30MinWindow).time = Some(tripEvent.endTime)
+                case timeDiff if timeDiff > 0 => 
+                  head_30MinWindow = (head_30MinWindow+timeDiff.toInt) % circularBufferSize
+                  circularBuffer(head_30MinWindow).time = Some(tripEvent.endTime)
+                  circularBuffer(head_30MinWindow).tripEventStack.push(tripEvent)
+                  
+                  val oldTailPosition = tail_30MinWindow
+                  if((circularBuffer(head_30MinWindow).time.get.getTime() - circularBuffer(tail_30MinWindow).time.get.getTime())/1000 > circularBufferSize ) {
+                    val tailJump = ((circularBuffer(head_30MinWindow).time.get.getTime() - circularBuffer(tail_30MinWindow).time.get.getTime())/1000) - circularBufferSize
+                    tail_30MinWindow = (tail_30MinWindow+tailJump.toInt) % circularBufferSize
+                    circularBuffer(tail_30MinWindow).time.get.setTime(circularBuffer(tail_30MinWindow).time.get.getTime + tailJump.toInt*1000)
                   }
-                }
+                  
+                  if(oldTailPosition < tail_30MinWindow-1) {
+                    for(y <- oldTailPosition until tail_30MinWindow) {
+                      for(z <- 0 until circularBuffer(y).tripEventStack.size) {
+                        val event = circularBuffer(y).tripEventStack.pop()
+                        routeCountActor ! RouteCountActor.DecrementRouteCountMsg(event)
+                      }
+                    }
+                  }  
+                  else if(oldTailPosition > tail_30MinWindow-1){
+                    for(y <- oldTailPosition until circularBufferSize) {
+                      for(z <- 0 until circularBuffer(y).tripEventStack.size) {
+                        val event = circularBuffer(y).tripEventStack.pop()
+                        routeCountActor ! RouteCountActor.DecrementRouteCountMsg(event)
+                      }
+                    }
+                    for(y <- 0 until tail_30MinWindow) {
+                      for(z <- 0 until circularBuffer(y).tripEventStack.size) {
+                        val event = circularBuffer(y).tripEventStack.pop()
+                        routeCountActor ! RouteCountActor.DecrementRouteCountMsg(event)
+                      }
+                    }
+                  }
               }
-              else if(oldTailPosition > tail.position-1){
-                for(y <- oldTailPosition until circularBufferSize) {
-                  val stackSize = circularBuffer(y).size
-                  for(z <- 0 to stackSize-1) {
-                    val event = circularBuffer(y).pop()
-                    //println("pop")
-                    routeCountActor ! RouteCountActor.DecrementRouteCountMsg(event)
-                  }
-                }
-                for(y <- 0 until tail.position) {
-                  val stackSize = circularBuffer(y).size
-                  for(z <- 0 to stackSize-1) {
-                    val event = circularBuffer(y).pop()
-                    //println("pop")
-                    routeCountActor ! RouteCountActor.DecrementRouteCountMsg(event)
-                  }
-                }
-              }
-              //println("Head=" + head.position + "      Tail=" + tail.position + "      Head_time= " + head.time.get + "      Tail_time= " + tail.time.get + "      Time_diff= " + ((head.time.get.getTime() - tail.time.get.getTime())/1000))
-                
-            
-              case x if x < 0 => 
-                //println("incoming_trip_time LESSER => head_time: " + head.time + ", incoming_trip_time: " + tripEvent.endTime)
+              case timeDiff if timeDiff < 0 => 
+                println("incoming_trip_time LESSER => head_time: " + (circularBuffer(head_30MinWindow)).time + ", incoming_trip_time: " + tripEvent.endTime)
                 //ignore
-                
-            }
-            
-            
-            
-            
-        }
-     */
-        
-        newlineCount += 1
+          }    
+    
+          
+      }  
+      newlineCount += 1
     }
-    log.info("In CircularBuffer - count is " + newlineCount.toString)
+    println("In CircularBuffer - count is " + newlineCount.toString)
   }
   
   def init() {
     for(i <- 0 until circularBufferSize)
-      circularBuffer(i) = BufferEntry(None, new Stack[TripEvent])
+      circularBuffer(i) = new BufferEntry(None, new Stack[TripEvent])
   }
   
   
