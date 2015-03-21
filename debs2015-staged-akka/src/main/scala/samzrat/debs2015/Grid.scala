7eff802d63523eff4caaa39f16e3da2f3374e9ba
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2015-03-21 11:07:59 -0600

    grid computation changes

--- debs2015-staged-akka/src/main/scala/samzrat/debs2015/Grid.scala
+++ debs2015-staged-akka/src/main/scala/samzrat/debs2015/Grid.scala
@@ -2,10 +2,17 @@ package samzrat.debs2015
 
 class Grid(cellSize: Int, firstCellCentreLongitude: Double, firstCellCentreLatitude: Double, maxCell: Int) {
   
+  val XdegreesPer500 = 0.005986
+  val YdegreesPer500 = 0.004491556
   
+  val XdegreesPerCell = if(cellSize==500) XdegreesPer500 else XdegreesPer500/2.0
+  val YdegreesPerCell = if(cellSize==500) YdegreesPer500 else YdegreesPer500/2.0
+  
+  val origin_longitude = firstCellCentreLongitude - XdegreesPerCell/2.0
+  val origin_latitude  = firstCellCentreLatitude + YdegreesPerCell/2.0
   
   def getCell(longitute: Double, latitude: Double): Option[Cell] = {
-    val cell: Cell = Cell(((longitute-(firstCellCentreLongitude-(cellSize.toDouble/2)))/cellSize).toInt + 1, ((latitude-(firstCellCentreLatitude-(cellSize.toDouble/2)))/cellSize).toInt + 1)  
+    val cell: Cell = Cell(((longitute-origin_longitude)/XdegreesPerCell).toInt + 1, ((origin_latitude-latitude)/YdegreesPerCell).toInt + 1)  
     if(cell.xCell<1 || cell.yCell<1 || cell.xCell>maxCell || cell.yCell>maxCell)
       None
     else
